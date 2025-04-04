1b5dedf18a2ae6b4f7ec87ae110d5f972c9fe507
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-04 11:56:58 +0200

    feat: add runtime configuration and logging utility

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,19 +3,24 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/ui',
-    '@nuxt/eslint',
-    '@nuxt/icon',
-    '@nuxt/fonts',
-    '@nuxt/image',
-    '@nuxt/scripts'
+    "@nuxt/ui",
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@nuxt/fonts",
+    "@nuxt/image",
+    "@nuxt/scripts",
   ],
 
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   future: {
-    compatibilityVersion: 4
+    compatibilityVersion: 4,
   },
 
-  compatibilityDate: '2024-11-27'
-})
+  compatibilityDate: "2024-11-27",
+  runtimeConfig: {
+    accessToken:
+      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOWE5N2E2YWMxZmE0ZTFjM2MwNDBjOTMwMDhiZjcwNSIsIm5iZiI6MTc0Mzc1OTI4MC4yODE5OTk4LCJzdWIiOiI2N2VmYTdiMDQ4ZDZmMDMxYjBhY2ZlYTAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.JzkzmCY_S6PvAnBXPrFB46TQgrjQU3ppHiBt09IsIgU",
+    apiKey: "09a97a6ac1fa4e1c3c040c93008bf705",
+  },
+});
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const config = useRuntimeConfig();
+
+pr(config.public.accessToken, "accessToken");
+pr(config.public.apiKey, "apiKey");
+</script>
 <template>
   <div>Main Page</div>
 </template>
