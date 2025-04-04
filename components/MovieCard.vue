ffefd876e6e24b30d61d8b2ecdc49c25572c4adf
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-04 13:51:28 +0200

    feat: enhance MovieCard component with TypeScript props and add Movie interface

--- components/MovieCard.vue
+++ components/MovieCard.vue
@@ -1,4 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { Movie } from "~/types";
+
+defineProps<{
+  movie: Movie;
+}>();
+
+const imageUrl = (path: string) => {
+  return `https://image.tmdb.org/t/p/w500${path}`;
+};
+</script>
 <template>
   <div />
 </template>
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,8 @@ import withNuxt from "./.nuxt/eslint.config.mjs";
 export default withNuxt({
   rules: {
     "vue/multi-word-component-names": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "off",
   },
 });
 // Your custom configs here
