718352fa0c25ceaf4ca2436dd0b0230466602a36
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-04 14:23:19 +0200

    feat: update MovieCard component with enhanced template and styling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,7 @@ export default withNuxt({
     "vue/multi-word-component-names": "off",
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": "off",
+    "vue/html-self-closing": "off",
   },
 });
 // Your custom configs here
