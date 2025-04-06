aea758e360d24db2e59e5b413003195f126eba8e
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-07 00:40:55 +0200

    feat: update SeriesCard to display first air date for TV shows and add first_air_date to Movie type

--- components/SeriesCard.vue
+++ components/SeriesCard.vue
@@ -40,7 +40,7 @@ const { movie: choosenMovie, chooseMovie } = useChoosenMovie();
       <div
         class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
-        {{ movie.release_date }}
+        {{ movie?.first_air_date }}
         <svg
           class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
           aria-hidden="true"
