e77d1983e34caa746141f923d7da58ac028433f3
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-07 21:02:47 +0200

    feat: implement search functionality for movies and TV series; add API integration

---
+++ server/api/movies/search.ts
@@ -0,0 +1,31 @@
+import type { ApiResponse, Movie } from "~/types";
+
+export default defineEventHandler(async (e) => {
+  const query = getQuery(e);
+  const { accessToken } = useRuntimeConfig();
+  const searchMoviesUrl = "https://api.themoviedb.org/3/search/movie";
+  const searchSeriesUrl = "https://api.themoviedb.org/3/search/tv";
+  const movies = await $fetch<ApiResponse<Movie>>(searchMoviesUrl, {
+    method: "get",
+    params: {
+      language: "en-US",
+      query: query.search,
+    },
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      accept: "application/json",
+    },
+  });
+  const series = await $fetch<ApiResponse<Movie>>(searchSeriesUrl, {
+    method: "get",
+    params: {
+      language: "en-US",
+      query: query.search,
+    },
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      accept: "application/json",
+    },
+  });
+  return { movies };
+});
