b7cd46b3a51b3bf4eba8583187ef6130b8246f53
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-07 20:43:29 +0200

    feat: implement popular movies API endpoint and enhance event handler for fetching movie data

--- server/api/movies/popular.ts
+++ server/api/movies/popular.ts
@@ -1,5 +1,21 @@
-export default defineEventHandler((e) => {
+import type { ApiResponse, Movie } from "~/types";
+
+export default defineEventHandler(async (e) => {
   const { accessToken } = useRuntimeConfig();
-  const poularUrl = "";
-  return {};
+  const popularUrl = "https://api.themoviedb.org/3/movie/popular";
+  const movies = await $fetch<ApiResponse<Movie>>(
+    "https://api.themoviedb.org/3/movie/popular",
+    {
+      method: "get",
+      params: {
+        language: "en-US",
+        page: 1,
+      },
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        accept: "application/json",
+      },
+    }
+  );
+  return { movies };
 });
