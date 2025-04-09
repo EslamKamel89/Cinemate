28529ba7206e39ce3cd036659686b680a95b3b6d
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-09 20:01:41 +0200

    feat: implement upcoming movies API integration; add fetching and response handling

---
+++ server/api/movies/upcoming.ts
@@ -0,0 +1,19 @@
+import type { ApiResponse, Movie } from "~/types";
+
+export default defineEventHandler(async (e) => {
+  const { accessToken } = useRuntimeConfig();
+  const upcomingMoivesUrl = "https://api.themoviedb.org/3/movie/upcoming";
+  const movies = await $fetch<ApiResponse<Movie>>(upcomingMoivesUrl, {
+    method: "get",
+    params: {
+      language: "en-US",
+      page: 1,
+    },
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      accept: "application/json",
+    },
+  });
+
+  return { movies };
+});
